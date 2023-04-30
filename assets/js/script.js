7e6c09e26017c2c7a3e194d5d6ba990c4340278c
Gabries <105074547+gabrieszin@users.noreply.github.com>
2023-04-30 16:38:43 -0300

    Implementando primeiros scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,17 @@ import { SwalAlert, isEmpty } from './modulos/utilitarios.js';
 (() => {
   hljs.highlightAll();
   
-  $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
+  const renderTooltips = () => {
+    $(function () {
+      $('[data-toggle="tooltip"]').tooltip()
+    })
+  }
   
-  $(document).ready(function(){
-    $('[data-bs-toggle="popover"]').popover();  
-  });
+  const renderPopover = () => {
+    $(document).ready(function(){
+      $('[data-bs-toggle="popover"]').popover();  
+    });
+  }
   
   document.querySelectorAll('[data-recarrega-pagina]').forEach(botao => {
     botao.addEventListener('click', () => {
@@ -35,6 +39,9 @@ import { SwalAlert, isEmpty } from './modulos/utilitarios.js';
     document.querySelector('[data-action="incluir-proponente"]').addEventListener('click', (evento) => {
       document.querySelector('.accordion').innerHTML += `${conteudos.accordion_item(document.querySelectorAll('.accordion-item').length + 1)}`;
       clickRemoverProponente();
+      edicaoInputNome();
+      renderTooltips();
+      renderPopover();
     })
   }
 
@@ -44,7 +51,7 @@ import { SwalAlert, isEmpty } from './modulos/utilitarios.js';
     botao.forEach(botao => {
       // removeEventListener('click', botao);
       botao.addEventListener('click', async (evento) => {
-        SwalAlert('confirmacao', 'question', 'Tem certeza que deseja remover?', 'Depois de remover esta ação não poderá ser desfeita').then((retorno) => {
+        SwalAlert('confirmacao', 'question', 'Tem certeza que deseja remover?', 'Esta ação não poderá ser desfeita').then((retorno) => {
           if(retorno.isConfirmed){
             evento.target.closest('[data-identify]').remove();
           }
@@ -53,7 +60,20 @@ import { SwalAlert, isEmpty } from './modulos/utilitarios.js';
     })
   }
 
+  const edicaoInputNome = () => {
+    document.querySelectorAll('[data-input="nome"]').forEach(input => {
+      input.addEventListener('input', () => {
+        input.closest('[data-identify]').querySelectorAll('[data-content="nome"]').forEach(content => {
+          content.textContent = !isEmpty(input.value) ? input.value : 'Nome do cliente';
+        }) 
+      })
+    })
+  }
+
+  renderTooltips();
+  renderPopover();
   clickIncluirProponente();
   clickRemoverProponente();
+  edicaoInputNome();
   
 })();
