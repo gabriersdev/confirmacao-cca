a5f5ef2b3fc0088d63ead231fc55e6889b7bde95
Gabries <105074547+gabrieszin@users.noreply.github.com>
2023-04-30 18:03:59 -0300

    Implementando primeiros scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,20 +34,21 @@ import { SwalAlert, isEmpty } from './modulos/utilitarios.js';
       })
     }
   }
-
+  
   const clickIncluirProponente = () => {
     document.querySelector('[data-action="incluir-proponente"]').addEventListener('click', (evento) => {
       document.querySelector('.accordion').innerHTML += `${conteudos.accordion_item(document.querySelectorAll('.accordion-item').length + 1)}`;
-      clickRemoverProponente();
-      edicaoInputNome();
       renderTooltips();
       renderPopover();
+      clickRemoverProponente();
+      edicaoInputNome();
+      renderPendencias();
     })
   }
-
+  
   const clickRemoverProponente = () => {
     const botao = document.querySelectorAll('[data-action="remover-proponente"]');
-
+    
     botao.forEach(botao => {
       // removeEventListener('click', botao);
       botao.addEventListener('click', async (evento) => {
@@ -59,7 +60,7 @@ import { SwalAlert, isEmpty } from './modulos/utilitarios.js';
       })
     })
   }
-
+  
   const edicaoInputNome = () => {
     document.querySelectorAll('[data-input="nome"]').forEach(input => {
       input.addEventListener('input', () => {
@@ -69,11 +70,45 @@ import { SwalAlert, isEmpty } from './modulos/utilitarios.js';
       })
     })
   }
-
+  
+  const renderPendencias = () => {
+    const proponentes = document.querySelectorAll('.accordion-item');
+    proponentes.forEach(proponente => {
+      const inputs = proponente.querySelectorAll('[data-element="input"]');
+      
+      const elementos = {
+        nome : get('nome'),
+        cpf : get('cpf'),
+        data_nascimento : get('data_nascimento'),
+        email : get('email'),
+        telefone : get('telefone'),
+        endereco : get('endereco'),
+        tipo_endereco : get('tipo_endereco'),
+        renda : get('renda'),
+        tipo_renda : get('tipo_renda'),
+        comprovante_estado_civil : get('comprovante_estado_civil'),
+        fgts : get('fgts'),
+      }
+      
+      console.log(elementos)
+      
+      function get(id){
+        const saida = new Array();
+        inputs.forEach(input => {
+          input.dataset.input == `${id}` ? saida.push(input.value) : '';
+        })
+        return saida;
+      }
+    })
+  }
+  
+  
   renderTooltips();
   renderPopover();
+  document.querySelector('.accordion').innerHTML += `${conteudos.accordion_item(1)}`;
   clickIncluirProponente();
   clickRemoverProponente();
   edicaoInputNome();
+  renderPendencias();
   
 })();
