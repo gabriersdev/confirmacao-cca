d3918ed1296f99f706fa99fc01d053ebeef5fb48
Gabries <105074547+gabrieszin@users.noreply.github.com>
2023-05-14 09:19:01 -0300

    Removendo frameworks desnecessários, atualizando README e corrigindo BUG

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Confirmação Dados CCA
 
+O projeto é uma ferramenta que visa ajudar Correspondentes Bancários em Processos Habitacionais. 
+
+## Como funciona?
+
+Na página de [confirmação de dados](), o CCA registra os dados recebidos do(s) proponentes, e conforme forem os dados, a ferramenta informa o que está ou não pendente e o que precisa ser apresentado ou avaliado sobre a utilização no processo. 
+
+É possível gerar e baixar um arquivo `.txt` com os principais dados dos proponentes (nome, CPF, data de nascimento, telefone e e-mail). Também é possível editar e posteriormente baixar um relatório sobre o processo. 
+
+As pendências documentais aparecem que foram identificadas pela ferramenta aparecem no campo "Pendências", que pode ser editado, copiado ou baixado (o arquivo gerado é no formato `.txt`).
+
+## Bibliotecas e Frameworks
+
+- Bootstrap
+- Bootstrap Icons
+- jQuery
+- FileSaver
+
+## Tecnologias Utilizadas
+
+- JavaScript
+- CSS
+- HTML
+- Figma
--- assets/js/modulos/funcoes-render.js
+++ assets/js/modulos/funcoes-render.js
@@ -39,7 +39,7 @@ const renderFeedbacks = (proponente) => {
             mensagem = 'Necessário apresentar documento atualizado.'
           }
           break;
-
+          
           case 'irpf':
           case 'contrato de aluguel':
           mensagem = 'Documento não é aceito para os produtos CCFGTS e PMCMV.';
@@ -55,29 +55,29 @@ const renderFeedbacks = (proponente) => {
       break;
       
       case 'feedback-renda':
-        const tipos_rendas = new Array();
-        elementos[index].forEach(elemento => {
-          tipos_rendas.push(elemento.value.trim().toLowerCase());
+      const tipos_rendas = new Array();
+      elementos[index].forEach(elemento => {
+        tipos_rendas.push(elemento.value.trim().toLowerCase());
+      })
+      
+      //Verifica se existem valores vazios
+      if(tipos_rendas.findIndex(e => e == '') !== -1){
+        mensagem = 'Necessário informar renda.';
+      }
+      if(tipos_rendas.includes('contracheque/hollerith') || tipos_rendas.includes('irpf')){
+        proponente.querySelectorAll('[data-input="renda_valida"]').forEach(renda_valida => {
+          if(!renda_valida.checked){
+            mensagem = 'Necessário apresentar documento atualizado.';
+          }
         })
-
-        //Verifica se existem valores vazios
-        if(tipos_rendas.findIndex(e => e == '') !== -1){
-          mensagem = 'Necessário informar renda.';
-        }
-        if(tipos_rendas.includes('contracheque/hollerith') || tipos_rendas.includes('irpf')){
-          proponente.querySelectorAll('[data-input="renda_valida"]').forEach(renda_valida => {
-            if(!renda_valida.checked){
-              mensagem = 'Necessário apresentar documento atualizado.';
-            }
-          })
-        }
-        if(tipos_rendas.includes('contrato de aluguel') || tipos_rendas.includes('extratos bancários')){
-          mensagem = 'Necessário formalizar a renda.'
-        }
-        if(tipos_rendas.includes('outro')){
-          mensagem = 'Verifique o Manual Normativo do Produto sobre a utilização deste documento.'
-        }
-
+      }
+      if(tipos_rendas.includes('contrato de aluguel') || tipos_rendas.includes('extratos bancários')){
+        mensagem = 'Necessário formalizar a renda.'
+      }
+      if(tipos_rendas.includes('outro')){
+        mensagem = 'Verifique o Manual Normativo do Produto sobre a utilização deste documento.'
+      }
+      
       break;
     }
     
@@ -87,84 +87,86 @@ const renderFeedbacks = (proponente) => {
 
 const renderPendencias = () => {
   const txt = document.querySelector('[data-content="pendencias"]');
-  txt.value = '';
-  const pendencias = new Array();
-  
-  const proponentes = document.querySelectorAll('.accordion-item');
-  if(!isEmpty(proponentes) && proponentes.length > 0){
-    proponentes.forEach((proponente, index) => {
-      const inputs = proponente.querySelectorAll('[data-element="input"]');
-      
-      const elementos = {
-        valores: {
-          nome : get('nome'),
-          cpf : get('cpf'),
-          data_nascimento : get('data_nascimento'),
-          email : get('email'),
-          telefone : get('telefone'),
-          endereco : get('endereco'),
-          // tipo_endereco : get('tipo_endereco'),
-          endereco_valido: get('endereco_valido'),
-          renda : get('renda'),
-          renda_valida: get('renda_valida'),
-          // tipo_renda : get('tipo_renda'),
-          comprovante_estado_civil : get('comprovante_estado_civil'),
-          fgts : get('fgts'),
-        },
-        some(parametro){
-          const retorno = new Array();
-          for (const [key, value] of Object.entries(this.valores)){
-            if(value == parametro){retorno.push(key);}
-          }
-          return retorno.length > 0 ? true : false;
-        },
-        search(parametro){
-          const retorno = new Array();
-          for (const [key, value] of Object.entries(this.valores)){
-            if(Array.isArray(value)){
-              value.forEach((v, index) => {
-                if(v == parametro && !retorno.includes(key)){
-                  if(key !== 'fgts'){
-                    retorno.push(key)
-                  };
-                }
-              })
-            }else{
-              if(value == parametro){key !== 'fgts' ? retorno.push(key) : '';}
-            }
-          }
-          return retorno;
-        }
-      }
-      
-      if(elementos.some('')){
-        pendencias.push({proponente: `PROPONENTE ${index + 1} - ${elementos.valores.nome}`, pendente: elementos.search('').join().replaceAll('_', ' ').toUpperCase().replaceAll(',', '\n')})
-      }
-      
-      function get(id){
-        const saida = new Array();
-        inputs.forEach(input => {
-          if(input.type == 'checkbox' && input.dataset.input == `${id}`){
-            input.checked ? saida.push(true) : saida.push('');
-          }else{
-            input.dataset.input == `${id}` ? saida.push(input.value) : '';
-          }
-        })
-        return saida;
-      }
-      
-      renderFeedbacks(proponente);
-    })
-    
+  if(!isEmpty(txt)){
     txt.value = '';
-    pendencias.forEach((pendencia, index) => {
-      // console.log(pendencia);
-      index !== 0? txt.value += '\n\n' : 'a';
-      // console.log('atualizado');
-      txt.value += `${pendencia.proponente}\n${pendencia.pendente}`
-    })
-    resizeTextArea(txt);
-    // txt.textContent = `${pendencias.forEach(pendencia => {return pendencia.pendente})}`;
+    const pendencias = new Array();
+    
+    const proponentes = document.querySelectorAll('.accordion-item');
+    if(!isEmpty(proponentes) && proponentes.length > 0){
+      proponentes.forEach((proponente, index) => {
+        const inputs = proponente.querySelectorAll('[data-element="input"]');
+        
+        const elementos = {
+          valores: {
+            nome : get('nome'),
+            cpf : get('cpf'),
+            data_nascimento : get('data_nascimento'),
+            email : get('email'),
+            telefone : get('telefone'),
+            endereco : get('endereco'),
+            // tipo_endereco : get('tipo_endereco'),
+            endereco_valido: get('endereco_valido'),
+            renda : get('renda'),
+            renda_valida: get('renda_valida'),
+            // tipo_renda : get('tipo_renda'),
+            comprovante_estado_civil : get('comprovante_estado_civil'),
+            fgts : get('fgts'),
+          },
+          some(parametro){
+            const retorno = new Array();
+            for (const [key, value] of Object.entries(this.valores)){
+              if(value == parametro){retorno.push(key);}
+            }
+            return retorno.length > 0 ? true : false;
+          },
+          search(parametro){
+            const retorno = new Array();
+            for (const [key, value] of Object.entries(this.valores)){
+              if(Array.isArray(value)){
+                value.forEach((v, index) => {
+                  if(v == parametro && !retorno.includes(key)){
+                    if(key !== 'fgts'){
+                      retorno.push(key)
+                    };
+                  }
+                })
+              }else{
+                if(value == parametro){key !== 'fgts' ? retorno.push(key) : '';}
+              }
+            }
+            return retorno;
+          }
+        }
+        
+        if(elementos.some('')){
+          pendencias.push({proponente: `PROPONENTE ${index + 1} - ${elementos.valores.nome}`, pendente: elementos.search('').join().replaceAll('_', ' ').toUpperCase().replaceAll(',', '\n')})
+        }
+        
+        function get(id){
+          const saida = new Array();
+          inputs.forEach(input => {
+            if(input.type == 'checkbox' && input.dataset.input == `${id}`){
+              input.checked ? saida.push(true) : saida.push('');
+            }else{
+              input.dataset.input == `${id}` ? saida.push(input.value) : '';
+            }
+          })
+          return saida;
+        }
+        
+        renderFeedbacks(proponente);
+      })
+      
+      txt.value = '';
+      pendencias.forEach((pendencia, index) => {
+        // console.log(pendencia);
+        index !== 0? txt.value += '\n\n' : 'a';
+        // console.log('atualizado');
+        txt.value += `${pendencia.proponente}\n${pendencia.pendente}`
+      })
+      resizeTextArea(txt);
+      // txt.textContent = `${pendencias.forEach(pendencia => {return pendencia.pendente})}`;
+    }
   }
 }
 
