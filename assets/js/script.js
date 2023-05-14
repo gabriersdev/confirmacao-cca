e91d7d02cb15cb3609d93d198ba5281a27351073
Gabries <105074547+gabrieszin@users.noreply.github.com>
2023-05-14 09:37:00 -0300

    Corrigindo bug de carramento da página

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,9 @@ import { funcoesBase } from './modulos/funcoes-base.js';
   const pagina = new URL(window.location).pathname.trim().replace('/', '');
   const body = document.querySelector('body');
   
-  if(pagina == 'index.html'){
+  console.log(pagina, isEmpty(pagina))
+
+  if(pagina == 'index.html' || isEmpty(pagina)){
     body.innerHTML += conteudos.conteudo_pagina_confirmacao;
     const accordion_item = document.createElement('div');
     accordion_item.classList.value = 'accordion-item';
