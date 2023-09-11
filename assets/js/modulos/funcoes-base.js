e957a5b92b303e9baa21781f976cb9187259b777
Gabriel <105074547+gabrieszin@users.noreply.github.com>
2023-09-11 07:02:47 -0300

    Add. máscara para matrícula

--- assets/js/modulos/funcoes-base.js
+++ assets/js/modulos/funcoes-base.js
@@ -91,6 +91,10 @@ const tratamentoCampos = (input) => {
       mascararValores(input);
       break;
       
+      case 'matricula':
+      $(input).mask('0000000');
+      break;
+      
       default:
       break;
     }
@@ -103,170 +107,170 @@ const tratamentoCampos = (input) => {
     
     function mascararValores(input){
       // Créditos https://stackoverflow.com/questions/62894283/javascript-input-mask-currency
-
+      
       if(isEmpty(input.value)){
         input.value = 'R$ 0,00';
       }
-
+      
       input.addEventListener('input', () => {
         const value = input.value.replace('.', '').replace(',', '').replace(/\D/g, '')
-      
+        
         const options = { minimumFractionDigits: 2 }
         const result = new Intl.NumberFormat('pt-BR', options).format(
           parseFloat(value) / 100
-        )
+          )
+          
+          if(isNaN(result) && result == 'NaN'){
+            input.value = 'R$ 0,00';
+          }else{
+            input.value = 'R$ ' + result;
+          }
+        })
         
-        if(isNaN(result) && result == 'NaN'){
-          input.value = 'R$ 0,00';
-        }else{
-          input.value = 'R$ ' + result;
-        }
-      })
-
-      // SimpleMaskMoney.setMask(input, {
-      //   prefix: 'R$ ',
-      //   fixed: true,
-      //   fractionDigits: 2,
-      //   decimalSeparator: ',',
-      //   thousandsSeparator: '.',
-      //   cursor: 'move'
-      // });
-      input.removeAttribute('maxlength');
-      
-      // input.addEventListener('input', (evento) => {
-      //   const value = evento.target.value;
-      
-      //   const split = value.trim().split(' ').filter(e => e !== ' ');
-      //   const convertido = (split.map(e => parseInt(e)));
-      //   let valor = (convertido.find(e => !isNaN(e))).toString();
-      //   valor = '' + valor;
-      //   // console.log(valor.substring(0, valor.lenght - 2) + valor.susbtring(valor.lenght - 2, valor.lenght));
-      
-      //   // console.log(valor);
-      
-      //   // console.log(SimpleMaskMoney.formatToCurrency(), { prefix: 'R$' }));
-      //   // evento.target.value = 
-      // })
-    }
-    
-  });
-}
-
-function funcoesBase(){
-  verificarInputsRecarregamento();
-  renderTooltips();
-  renderPopover();
-  renderResumo();
-  renderPendencias();
-  clickCopiar();
-  clickLimparProcesso();
-  clickIncluirProponente();
-  clickRemoverProponente();
-  clickRemoverRenda();
-  clickAddInformacoes();
-  clickVisibilidadeSenha();
-  clickAddDevolucaoFID();
-  submitAddDevolucaoFID();
-  submitInformarRestricoes();
-  edicaoInputNome();
-  escutaEventoInput();
-  clickImportarPendencias();
-  clickAcionarModal();
-  clickLimparTudoSecao();
-  clickEnviarDados();
-  
-  $('[data-action="exibir-informacoes"]').on('click', (evento) => {
-    evento.preventDefault();
-    document.querySelector('.modal-informacoes-pagina').showModal();
-  })
-  
-  $('[data-action=fechar-modal-dialog]').on('click', (evento) => {
-    evento.target.closest('dialog').close();
-  })
-  
-  let click113 = false;
-  
-  document.addEventListener('keyup', (evento) => {
-    const code = evento.keyCode;
-    if(!isEmpty(code)){
-      // console.log(code)
-      if(code == 45){
-        acaoClickIncluirProponente()
-      }else if(code == 113){
-        click113 = true;
+        // SimpleMaskMoney.setMask(input, {
+        //   prefix: 'R$ ',
+        //   fixed: true,
+        //   fractionDigits: 2,
+        //   decimalSeparator: ',',
+        //   thousandsSeparator: '.',
+        //   cursor: 'move'
+        // });
+        input.removeAttribute('maxlength');
         
-        setTimeout(() => {
-          click113 = false;
-        }, 1000)
-      }else if(code == 68 && click113){
-        // Baixar arquivo de dados
-        clickDownload({dataset: {download: 'baixar-dados'}}, evento);
-      }else if(code == 70 && click113){
-        // Ir para Acompanhar FID
-        $('[data-element="input-URL-acompanhar-FID"]').focus();
-      }else if(code == 80 && click113){
-        // Baixar arquivo de pendências
-        clickDownload({dataset: {download: 'baixar-pendencias'}}, evento);
-      }else if(code == 82 && click113){
-        // Baixar arquivo de relatório
-        clickDownload({dataset: {download: 'baixar-relatorio'}}, evento);
-      }else if(code == 86 && click113){
-        acionarDevolucaoFID();
-      }else if(code == 73 && click113){
-        acionarModalAddInformacoes();
+        // input.addEventListener('input', (evento) => {
+        //   const value = evento.target.value;
+        
+        //   const split = value.trim().split(' ').filter(e => e !== ' ');
+        //   const convertido = (split.map(e => parseInt(e)));
+        //   let valor = (convertido.find(e => !isNaN(e))).toString();
+        //   valor = '' + valor;
+        //   // console.log(valor.substring(0, valor.lenght - 2) + valor.susbtring(valor.lenght - 2, valor.lenght));
+        
+        //   // console.log(valor);
+        
+        //   // console.log(SimpleMaskMoney.formatToCurrency(), { prefix: 'R$' }));
+        //   // evento.target.value = 
+        // })
       }
-    }
-  })
-  
-  const linksFaceis = $('.links-faceis-confirmacao');
-  if(!isEmpty(linksFaceis)){
-    [conteudos.consultas[9], conteudos.consultas[10], conteudos.consultas[11]].forEach(conteudo => {
-      $('.links-faceis-confirmacao').append(`<a class="card" href="${conteudo.link}" target="_blank" data-item="card-link-facil" rel="noreferrer noopener" data-toggle="tooltip" data-placement="top" title="Clique para abrir ->"><div class="card-header">${conteudo.sistema}<i class="bi bi-arrow-up-right-square" data-icon="icone"></i></div><div class="card-body"><b>${conteudo.titulo}</b></div></a>`);
-    });
-    
-    document.querySelectorAll('[data-item="card-link-facil"]').forEach(link => {
-      $(link).on('mousemove focus', () => {
-        link.querySelector('[data-icon="icone"]').classList.value = 'bi bi-arrow-up-right-square-fill';
-      })
       
-      $(link).on('mouseout blur', () => {
-        link.querySelector('[data-icon="icone"]').classList.value = 'bi bi-arrow-up-right-square';
-      })
-    })
+    });
   }
   
-  const btnCarregarPendencias = document.querySelector('[data-action="carregar-pendencias"]');
-  !isEmpty(btnCarregarPendencias) ? btnCarregarPendencias.onclick = carregarPendencias : '';
-  
-  function carregarPendencias(evento){
-    evento.preventDefault();
-    $(evento.target).tooltip('hide');
-    
-    text_areas_editados(false);
+  function funcoesBase(){
+    verificarInputsRecarregamento();
+    renderTooltips();
+    renderPopover();
+    renderResumo();
     renderPendencias();
+    clickCopiar();
+    clickLimparProcesso();
+    clickIncluirProponente();
+    clickRemoverProponente();
+    clickRemoverRenda();
+    clickAddInformacoes();
+    clickVisibilidadeSenha();
+    clickAddDevolucaoFID();
+    submitAddDevolucaoFID();
+    submitInformarRestricoes();
+    edicaoInputNome();
+    escutaEventoInput();
+    clickImportarPendencias();
+    clickAcionarModal();
+    clickLimparTudoSecao();
+    clickEnviarDados();
+    
+    $('[data-action="exibir-informacoes"]').on('click', (evento) => {
+      evento.preventDefault();
+      document.querySelector('.modal-informacoes-pagina').showModal();
+    })
+    
+    $('[data-action=fechar-modal-dialog]').on('click', (evento) => {
+      evento.target.closest('dialog').close();
+    })
+    
+    let click113 = false;
+    
+    document.addEventListener('keyup', (evento) => {
+      const code = evento.keyCode;
+      if(!isEmpty(code)){
+        // console.log(code)
+        if(code == 45){
+          acaoClickIncluirProponente()
+        }else if(code == 113){
+          click113 = true;
+          
+          setTimeout(() => {
+            click113 = false;
+          }, 1000)
+        }else if(code == 68 && click113){
+          // Baixar arquivo de dados
+          clickDownload({dataset: {download: 'baixar-dados'}}, evento);
+        }else if(code == 70 && click113){
+          // Ir para Acompanhar FID
+          $('[data-element="input-URL-acompanhar-FID"]').focus();
+        }else if(code == 80 && click113){
+          // Baixar arquivo de pendências
+          clickDownload({dataset: {download: 'baixar-pendencias'}}, evento);
+        }else if(code == 82 && click113){
+          // Baixar arquivo de relatório
+          clickDownload({dataset: {download: 'baixar-relatorio'}}, evento);
+        }else if(code == 86 && click113){
+          acionarDevolucaoFID();
+        }else if(code == 73 && click113){
+          acionarModalAddInformacoes();
+        }
+      }
+    })
+    
+    const linksFaceis = $('.links-faceis-confirmacao');
+    if(!isEmpty(linksFaceis)){
+      [conteudos.consultas[9], conteudos.consultas[10], conteudos.consultas[11]].forEach(conteudo => {
+        $('.links-faceis-confirmacao').append(`<a class="card" href="${conteudo.link}" target="_blank" data-item="card-link-facil" rel="noreferrer noopener" data-toggle="tooltip" data-placement="top" title="Clique para abrir ->"><div class="card-header">${conteudo.sistema}<i class="bi bi-arrow-up-right-square" data-icon="icone"></i></div><div class="card-body"><b>${conteudo.titulo}</b></div></a>`);
+      });
+      
+      document.querySelectorAll('[data-item="card-link-facil"]').forEach(link => {
+        $(link).on('mousemove focus', () => {
+          link.querySelector('[data-icon="icone"]').classList.value = 'bi bi-arrow-up-right-square-fill';
+        })
+        
+        $(link).on('mouseout blur', () => {
+          link.querySelector('[data-icon="icone"]').classList.value = 'bi bi-arrow-up-right-square';
+        })
+      })
+    }
+    
+    const btnCarregarPendencias = document.querySelector('[data-action="carregar-pendencias"]');
+    !isEmpty(btnCarregarPendencias) ? btnCarregarPendencias.onclick = carregarPendencias : '';
+    
+    function carregarPendencias(evento){
+      evento.preventDefault();
+      $(evento.target).tooltip('hide');
+      
+      text_areas_editados(false);
+      renderPendencias();
+    }
+    
+    const modal = document.querySelector('#modal-devolucao-fid');
+    if(!isEmpty(modal)){
+      modal.querySelectorAll('textarea').forEach(textarea => {
+        textarea.addEventListener('input', (evento) => {
+          resizeTextArea(textarea);
+        })
+      })
+    }
   }
   
-  const modal = document.querySelector('#modal-devolucao-fid');
-  if(!isEmpty(modal)){
-    modal.querySelectorAll('textarea').forEach(textarea => {
-      textarea.addEventListener('input', (evento) => {
-        resizeTextArea(textarea);
-      })
-    })
+  function atualizar(){
+    renderTooltips();
+    renderPopover();
+    renderPendencias();
+    escutaEventoInput();
+    atualizarNumerosProponentes();
   }
-}
-
-function atualizar(){
-  renderTooltips();
-  renderPopover();
-  renderPendencias();
-  escutaEventoInput();
-  atualizarNumerosProponentes();
-}
-
-export {
-  escutaEventoInput,
-  funcoesBase,
-  atualizar,
-  verificarInputsRecarregamento
-}
+  
+  export {
+    escutaEventoInput,
+    funcoesBase,
+    atualizar,
+    verificarInputsRecarregamento
+  }
